f3d8582468107a4e1d8ca5d565283ae552f25c3b
念卿 <96874574+v5r32qef@users.noreply.github.com>
2023-01-06 22:26:09 +0800

    Update 02.requests实战之破解百度翻译.py

--- 02.requests实战之破解百度翻译.py
+++ 02.requests实战之破解百度翻译.py
@@ -12,7 +12,7 @@ resp = requests.post(url, data=dat)
 print(resp.json())  # 将服务器返回的内容直接处理成json()  => dict
 #resp.text 源代码
 
-f = open("cat.json",mode="w",encoding="utf-8")
+f = open(".json",mode="w",encoding="utf-8")#点前面写要翻译的单词，要翻译什么就写什么
 json.dump(resp.json(),fp=f,ensure_ascii=False)
 #json.dumps 序列化时对中文默认使用的ascii编码.
 # 想输出真正的中文需要指定ensure_ascii=False
